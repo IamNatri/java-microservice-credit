d0d1bfa812dc006b3e0a21e26335d1546ea05d51
Matheus bastos <105763337+IamNatri@users.noreply.github.com>
2024-01-29 13:20:54 -0300

    wip initial commit

--- msclientes/src/main/java/com/github/IamNatri/msclientes/MsclientesApplication.java
+++ msclientes/src/main/java/com/github/IamNatri/msclientes/MsclientesApplication.java
@@ -5,15 +5,13 @@ import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Lazy;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsclientesApplication {
-
-	@Lazy
-	@Autowired
-	private EurekaClient eurekaClient;
 
 
 	public static void main(String[] args) {
